741af0327294d4fc5c469cb5dc87b57836f2b90c
pkishi <13582030+pkishi@users.noreply.github.com>
2020-10-26 00:57:40 -0700

    add new layer3 toggle

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cp -r keyboards/ ../qmk_firmware
-cd ../qmk_firmware
+cp -r keyboards/ qmk_firmware
+cd qmk_firmware
 make dz60:dz60arrow
-cp dz60_dz60arrow.hex ../dz60arrow
-cd ../dz60arrow
+cp dz60_dz60arrow.hex ..
+cd ..
 
